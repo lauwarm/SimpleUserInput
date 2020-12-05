5bb7bda837768961983334d0da61d75066681814
lauwarm <10065855+lauwarm@users.noreply.github.com>
2020-12-05 13:59:49 +0100

    Initial Commit

---
+++ SimpleUserInput/SimpleUserInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleUserInputApp.swift
+//  SimpleUserInput
+//
+//  Created by Fabian on 05.12.20.
+//
+
+import SwiftUI
+
+@main
+struct SimpleUserInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
